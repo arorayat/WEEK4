ef876075e0353dc4393fc5517a3ae9b6641835c7
arorayat <87564566+arorayat@users.noreply.github.com>
2021-09-17 19:56:28 +1000

    Add files via upload

---
+++ t4js.js
@@ -0,0 +1,26 @@
+const parent = new Vue({
+    el : '#class1',
+    data : {
+      
+        name : 'Adventures'
+    }
+})
+
+var app = new Vue({
+            el: '#app',
+            data: {
+                data: 60
+            }
+        })
+
+var obj = new Vue({
+  el: '#class-2',
+  data: {
+    items: [
+{ message: 'Day 1: Pickup From Delhi' },
+{ message: 'Day 2: Pickup From Delhi' },
+{ message: 'Day 3: Pickup From Delhi' },
+{ message: 'Day 4: Stay At Manali' }
+    ]
+  }
+})
